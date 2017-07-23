8abce8be15c8935315051447ea5cef25ddb3fff3
Ahsen Jaffer <ahsen.jaffer@gmail.com>
2017-07-23 17:16:52 +0000

    minor

--- train-gen.py
+++ train-gen.py
@@ -14,7 +14,8 @@ train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
 def get_image(source_path):
    filename = source_path.split('/')[-1]
-   path = DATA_FOLDER  + filename
+   path = DATA_FOLDER + 'IMG/'  + filename
+   #print(path)
    return cv2.imread(path)
 
 def shuffle(array):
@@ -47,6 +48,7 @@ def generator(samples, batch_size=32):
                 right_angle = center_angle - correction
 
                 images.extend([center_image, left_image, right_image])
+                #measurements.extend([center_angle, left_angle, right_angle])
                 measurements.append([center_angle, throttle, brake, speed])
                 measurements.append([left_angle, throttle, brake, speed])
                 measurements.append([right_angle, throttle, brake, speed])
@@ -57,11 +59,16 @@ def generator(samples, batch_size=32):
                 augmented_images.append(image)
                 augmented_measurements.append(measurement)
                 augmented_images.append(cv2.flip(image,1))
+                #augmented_measurements.append(measurement * - 1.0)
                 augmented_measurements.append([measurement[0] * - 1.0, measurement[1], measurement[2], measurement[3]])
 
             # trim image to only see section with road
             X_train = np.array(augmented_images)
             y_train = np.array(augmented_measurements)
+            #print("X_train.shape: {}".format(X_train.shape))
+            #print("y_train.shape: {}".format(y_train.shape))
+            #print(X_train)
+            #print(y_train)
             yield sklearn.utils.shuffle(X_train, y_train)
 
 # compile and train the model using the generator function
@@ -80,26 +87,26 @@ from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(row, col, ch)))
-# Preprocess incoming data, centered around zero with small standard deviation 
-model.add(Lambda(lambda x: x/127.5 - 1.,
-        input_shape=(row, col, ch),
-        output_shape=(row, col, ch)))
-# model.add(Lambda(lambda x: (x / 255) - 0.5, input_shape=(160,320,3)))
+#Preprocess incoming data, centered around zero with small standard deviation 
+#model.add(Lambda(lambda x: x/127.5 - 1.,
+#        input_shape=(row, col, ch),
+#        output_shape=(row, col, ch)))
+model.add(Lambda(lambda x: (x / 255) - 0.5, input_shape=(160,320,3)))
         
-model.add(Convolution2D(24, 5, 5, activation='relu'))
+model.add(Convolution2D(6, 5, 5, activation='relu'))
 model.add(MaxPooling2D())
 #model.add(Dropout(0.5))??
-model.add(Convolution2D(36, 5, 5, activation='relu'))
+model.add(Convolution2D(16, 5, 5, activation='relu'))
 model.add(MaxPooling2D())
-model.add(Convolution2D(38, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Convolution2D(64, 3, 3, activation='relu'))
-model.add(Convolution2D(64, 3, 3, activation='relu'))
+#model.add(Convolution2D(38, 3, 3, activation='relu'))
+#model.add(MaxPooling2D())
+#model.add(Convolution2D(64, 3, 3, activation='relu'))
+#model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(1))
+model.add(Dense(120))
+model.add(Dense(84))
+model.add(Dense(4))
 
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch= len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
