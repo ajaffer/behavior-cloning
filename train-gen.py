e80b59ba5c303f7f6fe7591d4b4df7b8d38af1a3
Ahsen Jaffer <ajaffer@pivotallabs.com>
2017-07-23 09:28:12 -0700

    minor

--- train-gen.py
+++ train-gen.py
@@ -75,11 +75,11 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(row, col, ch)))
 # Preprocess incoming data, centered around zero with small standard deviation 
 model.add(Lambda(lambda x: x/127.5 - 1.,
-        input_shape=(ch, row, col),
-        output_shape=(ch, row, col)))
+        input_shape=(row, col, ch),
+        output_shape=(row, col, ch)))
 # model.add(Lambda(lambda x: (x / 255) - 0.5, input_shape=(160,320,3)))
         
 model.add(Convolution2D(24, 5, 5, activation='relu'))
